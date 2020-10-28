a28aa33fbe93bdd24aca825be4e280ce94a32127
Tivon <cduu@vip.qq.com>
2020-10-28 15:23:25 +0800

    fix

--- src/pages/system/user/roles/RoleItem.tsx
+++ src/pages/system/user/roles/RoleItem.tsx
@@ -9,8 +9,8 @@ import Upsert from './Upsert';
 import AssignAccounts from './AssignAccounts';
 
 interface RoleItemPropTypes {
-    role?: RoleType
-    refresh: () => void
+    role?: RoleType;
+    refresh: () => void;
 }
 
 const RoleItem: React.FC<RoleItemPropTypes> = (props) => {
@@ -20,12 +20,15 @@ const RoleItem: React.FC<RoleItemPropTypes> = (props) => {
         Modal.confirm({
             title: '确定删除？',
             onOk: () => {
-                return delRole(currentRole.role_id as string).then(() => {
-                    message.success('操作成功');
-                    refresh();
-                }, err => {
-                    message.error(err.message);
-                });
+                return delRole(currentRole.role_id as string).then(
+                    () => {
+                        message.success('操作成功');
+                        refresh();
+                    },
+                    (err) => {
+                        message.error(err.message);
+                    },
+                );
             },
         });
     };
@@ -37,58 +40,52 @@ const RoleItem: React.FC<RoleItemPropTypes> = (props) => {
                 return setRoleStatus({
                     role_id: currentRole.role_id,
                     status: currentRole.status == 1 ? 2 : 1,
-                }).then(() => {
-                    message.success('操作成功');
-                    refresh();
-                }, err => {
-                    message.error(err.message);
-                });
+                }).then(
+                    () => {
+                        message.success('操作成功');
+                        refresh();
+                    },
+                    (err) => {
+                        message.error(err.message);
+                    },
+                );
             },
         });
     };
-    return (
-        role ?
-            <Card
-                className={styles.roleItem}
-                title={role.role_name}
-                extra={
-                    <div className={"text-muted"}>
-                        {role.role_user_count}
-                        <AssignAccounts role={role} onSuccess={refresh}>
-                            <UserSwitchOutlined className={styles.userIcon} />
-                        </AssignAccounts>
-                    </div>
-                }
-            >
-                <div className={"body"}>
-                    {role.description}
+    return role ? (
+        <Card
+            className={styles.roleItem}
+            title={role.role_name}
+            extra={
+                <div className={'text-muted'}>
+                    {role.role_user_count}
+                    <AssignAccounts role={role} onSuccess={refresh}>
+                        <UserSwitchOutlined className={styles.userIcon} />
+                    </AssignAccounts>
                 </div>
-                <Row className={styles.footer}>
-                    <Col span={8}>
-                        <Switch
-                            size={"small"}
-                            checked={role.status == 1}
-                            onChange={changeStatus}
-                        />
-                    </Col>
-                    <Col span={16} className={"text-right link-group"}>
-                        <a>
-                            <Upsert data={role} refresh={refresh}>
-                                <span>编辑</span>
-                            </Upsert>
-                        </a>
-                        <a onClick={del}>删除</a>
-                    </Col>
-                </Row>
+            }
+        >
+            <div>{role.description}</div>
+            <Row className={styles.footer}>
+                <Col span={8}>
+                    <Switch size={'small'} checked={role.status == 1} onChange={changeStatus} />
+                </Col>
+                <Col span={16} className={'text-right link-group'}>
+                    <a>
+                        <Upsert data={role} refresh={refresh}>
+                            <span>编辑</span>
+                        </Upsert>
+                    </a>
+                    <a onClick={del}>删除</a>
+                </Col>
+            </Row>
+        </Card>
+    ) : (
+        <Upsert refresh={refresh}>
+            <Card className={classNames(styles.roleItem, styles.addItem)}>
+                <PlusOutlined className={styles.addIcon} />
             </Card>
-            :
-            <Upsert refresh={refresh}>
-                <Card
-                    className={classNames(styles.roleItem, styles.addItem)}
-                >
-                    <PlusOutlined className={styles.addIcon} />
-                </Card>
-            </Upsert>
+        </Upsert>
     );
 };
 
--- src/pages/system/user/roles/Upsert.tsx
+++ src/pages/system/user/roles/Upsert.tsx
@@ -1,7 +1,7 @@
-import React  from 'react';
+import React from 'react';
 import ProForm, { DrawerForm, ProFormText, ProFormTextArea } from '@ant-design/pro-form';
 import { Form, message, Spin } from 'antd';
-import { connect,useRequest } from 'umi';
+import { connect, useRequest } from 'umi';
 import { ConnectProps, ConnectState } from '@/models/connect';
 import { ResponseError } from '@/utils/request';
 import { getRoleMenus } from '@/services/systemAccounts';
@@ -9,96 +9,101 @@ import RoleTreeSelect from './RoleTreeSelect';
 import { SysAccountRolesModelState } from './model';
 
 type IConnectState = ConnectState & {
-    sysAccountRolesModel: SysAccountRolesModelState
+    sysAccountRolesModel: SysAccountRolesModelState;
 };
 
-interface IUpsertProps extends ConnectProps{
-    data?: AnyObject
-    refresh: ()=>void
-    sysAccountRolesModel?: SysAccountRolesModelState
+interface IUpsertProps extends ConnectProps {
+    data?: AnyObject;
+    refresh: () => void;
+    sysAccountRolesModel?: SysAccountRolesModelState;
 }
 
-const Upsert:React.FC<IUpsertProps> = (props)=>{
-    const {children,data,refresh} = props;
+const Upsert: React.FC<IUpsertProps> = (props) => {
+    const { children, data, refresh } = props;
     const [form] = ProForm.useForm();
-    const {run:fetchRoleMenus,loading:fetchingRoles=false} = useRequest(getRoleMenus,{
+    const { run: fetchRoleMenus, loading: fetchingRoles = false } = useRequest(getRoleMenus, {
         manual: true,
-        throwOnError: true
-    })
-    const submit=(values:AnyObject)=>{
-        return props.dispatch({
-            type: 'sysAccountRolesModel/upsert',
-            payload: values
-        }).then(()=>{
-            refresh();
-            return true;
-        },(err:ResponseError)=>{
-            err.preventDefault();
-            message.error(err.message)
-        })
-    }
-    const onVisibleChange = (visible:boolean)=>{
-        if(visible){
-            if(data){
-                fetchRoleMenus(data.role_id).then((resList:any[])=>{
-                    data.res_list = resList.map(item=>item.res_id);
-                    form.setFieldsValue(data);
-                },err=>{
-                    message.error(err.message)
-                })
+        throwOnError: true,
+    });
+    const submit = (values: AnyObject) => {
+        return props
+            .dispatch({
+                type: 'sysAccountRolesModel/upsert',
+                payload: values,
+            })
+            .then(
+                () => {
+                    refresh();
+                    return true;
+                },
+                (err: ResponseError) => {
+                    err.preventDefault();
+                    message.error(err.message);
+                },
+            );
+    };
+    const onVisibleChange = (visible: boolean) => {
+        if (visible) {
+            if (data) {
+                fetchRoleMenus(data.role_id).then(
+                    (resList: any[]) => {
+                        data.res_list = resList.map((item) => item.res_id);
+                        form.setFieldsValue(data);
+                    },
+                    (err) => {
+                        message.error(err.message);
+                    },
+                );
             }
-        }else {
+        } else {
             // form.resetFields()
         }
-    }
+    };
     return (
         <DrawerForm
             trigger={children}
             onFinish={submit}
-            title={`${data?'编辑':'新增'}角色`}
+            title={`${data ? '编辑' : '新增'}角色`}
             labelCol={{ span: 6 }}
             wrapperCol={{ span: 18 }}
             form={form}
             onVisibleChange={onVisibleChange}
-            drawerProps={
-                {
-                    getContainer: document.body,
-                    width: 500,
-                    destroyOnClose: true,
-                }
-            }
+            drawerProps={{
+                getContainer: document.body,
+                width: 500,
+                destroyOnClose: true,
+            }}
         >
             <Spin spinning={fetchingRoles}>
-                <ProFormText hidden name={"role_id"} />
+                <ProFormText hidden name={'role_id'} />
                 <ProFormText
-                    name={"role_type"}
-                    label={"角色编码"}
+                    name={'role_type'}
+                    label={'角色编码'}
                     rules={[
-                        {required: true},
-                        {pattern: /a/,message: ''}
+                        { required: true },
+                        {
+                            pattern: /^[a-z]([a-z]|[0-9]|-)+$/i,
+                            message: '只能2位以上包含字母数字和-,且第一位不能是数字',
+                        },
                     ]}
-                    placeholder={"角色编码不能重复，将根据编码识别角色"}
+                    placeholder={'角色编码不能重复，将根据编码识别角色'}
                 />
                 <ProFormText
-                    name={"role_name"}
-                    label={"角色名"}
-                    rules={[{required: true}]}
-                    placeholder={"请输入角色名称"}
+                    name={'role_name'}
+                    label={'角色名'}
+                    rules={[{ required: true }]}
+                    placeholder={'请输入角色名称'}
                 />
-                <Form.Item label={"权限列表"} name={"res_list"} rules={[{required: true}]}>
+                <Form.Item label={'权限列表'} name={'res_list'} rules={[{ required: true }]}>
                     <RoleTreeSelect />
                 </Form.Item>
-                <ProFormTextArea
-                    name={"description"}
-                    label={"描述"}
-                    placeholder={"附加描述信息"}
-                />
+                <ProFormTextArea name={'description'} label={'描述'} placeholder={'附加描述信息'} />
             </Spin>
         </DrawerForm>
-    )
-}
+    );
+};
 
-const ConnectedUpsert = connect(({sysAccountRolesModel}: IConnectState) => ({
+const ConnectedUpsert = connect(({ sysAccountRolesModel }: IConnectState) => ({
     sysAccountRolesModel,
 }))(Upsert) as typeof Upsert;
 
